71dcf03d9b4840c98347d82110be1d8fe37764d8
szabiqqq <kucserka.szabolcs@hallgato.sze.hu>
2026-03-02 19:16:19 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int b = new int[N_ELEMENTS]; // NELEMENTS javitva
+    std::cout << "1-100 ertekek duplazasa\n"; //pontosvesszo
+    for (int i = 0; i < N_ELEMENTS; i++) //for hiba hianyz ofeltetel
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) 2; //hianyzo feltetel
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) //hibás feltétel
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl; //hianyzo cout
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; //atlag nincs inicializalva
+    for (int i = 0; i < N_ELEMENTS; i++) //pontos helyett sima vesszo
     {
-        atlag += b[i]
+        atlag += b[i]; //hianyzo pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; //dinamikus memoria hiba
     return 0;
 }
